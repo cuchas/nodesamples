a5126df2f6b4cb5bb32388ee5ef7af4ffc4063f8
eduardocucharro <eduardo.cucharro@gmail.com>
2014-03-29 16:54:14 -0300

    monitoring and child-process sample

---
+++ file-system/watcher.js
@@ -0,0 +1,9 @@
+//le o arquivo target.txt e fica monitorando ele para alterações
+
+const fs = require('fs');
+
+fs.watch('target.txt', function (){
+	console.log("File 'target.txt' just changed!");
+});
+
+console.log("Now watching target.txt for changes...");
